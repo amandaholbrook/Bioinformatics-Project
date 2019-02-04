2c216a8e67d85f8927b1154d84b5898bb785543d
Amanda Holbrook <amandajoee@gmail.com>
2019-02-04 16:37:28 -0500

    added detailed comments about future steps

--- main.py
+++ main.py
@@ -27,25 +27,50 @@ step = results.step #increment for gff file
 csv = results.csv #csv for renaming files in directory
 directory = results.directory #directory for renaming files
 
+assert fasta_file != None, "Please enter a fasta file (option -f)"
+
 filename = fasta_file[0:fasta_file.find('.')] #grabbing file name w/o extension
 formatted_fasta = 'formatted_' + fasta_file
 
 gff = filename + '.gff' #new gff file to be created, w/ old filename
 marker = ">" #marker for new sequence
 
-print("Step 1: Make sure fasta is properly formatted.\n")
+print("Make sure fasta is properly formatted.\n")
 format(fasta_file, formatted_fasta, marker)
 
-print("Step 2: Creating gff file.\n")
+print("Creating gff file.\n")
 createGFF(step, marker, formatted_fasta, gff)
+#Here we will use GffCompare to merge this GFF with a
+#user-provided one (if they provide it)
+#LOWEST PRIORITY:If we get EVERYTHING else done we'll
+#add on something here to add descriptors to the the
+#regions that don't code for genes.
 
-print("Step 3: Counts at each location.\n")
-print("Merge files and analyze.\n")
+print("Get counts at each location.\n")
+#Here we will use BWA (burrows wheeler aligner) to align
+#reads to fasta/GFF file. Then we will use HTseq-count to
+#convert alignments to counts
 
-print("Step 4: Differential counts per region.\n")
-print("Matrix dissection (DREME).\n")
+print("Merge files and analyze. (Optional)\n")
+#Here we will use the rename script you made (if the user
+#provided one)
 
-print("Step 5: Binding Matrix.\n")
-
-print("Another Step: Renaming files in directory\n")
+#if csv != "None":
+print("Renaming files in directory. (Optional)\n")
 #rename(folder_name, csv)
+
+print("Merging columns.\n")
+#Then we will merge columns using the merge counts script you made
+#merge(mergedfile, column, filetype, path)
+
+print("Differential counts per region.\n")
+#here we will run an R script (I can provide you with the
+#basic script that isn't properly automated yet anytime) to
+#produce differential counts for regions of interest. this will
+#be the primary output
+
+print("Binding Matrix/Matrix dissection (DREME). (Add On)\n")
+#This is the add on, we'll see if we get to this.  Ideally
+#we will pull all the sequences of regions that are significant
+#based on step 4 (and the IDs we created) and we'll run motif
+#analysis on them. This will be one of the primary outputs from the pipeline
