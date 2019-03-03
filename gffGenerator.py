51632eb06fc22615fb78b43c22abdee14a92d11d
Amanda Holbrook <amandajoee@gmail.com>
2019-03-03 14:40:31 -0500

    added more command line arguments && cleaned up files

--- gffGenerator.py
+++ gffGenerator.py
@@ -1,6 +1,6 @@
 import os, sys
 
-def createGFF(stepping, marker, formattedfasta, gff):
+def createGFF(stepping, marker, formattedfasta, gff, verbose):
 
 	first="TRUE"
 	f = open(formattedfasta)
@@ -10,25 +10,26 @@ def createGFF(stepping, marker, formattedfasta, gff):
 			line = lines.split() #splits the current line up into strings
 			id = line[0].replace(marker, "") #sets the first string as new id
 			seq="" #initiates a new seq
-			print("importing sequence", id, "....\n")
+			if verbose:
+				print("importing sequence", id, "....")
 
 		else:
 			seq=lines.rstrip()
 
 
 			region = (id + "\t" + "RefSeq" + "\t" + "region" + "\t" +  "1" +  "\t" + str(len(seq))\
-				+ "\t" + "." + "\t" + "+" + "\t" + "." + "\t" + "ID=" + id + ";Name=" + id + "\n")
+				+ "\t" + "." + "\t" + "+" + "\t" + "." + "\t" + "ID=" + id + "_region" + ";Name=" + id + "\n")
 			out.write(region) #prints for full region
 
 
 			for x in range(1, len(seq), stepping): #prints incrementing by preset step
-				next = x + stepping
+				next = x + stepping -1
 				if(next > len(seq)):
 					next = len(seq)
 				line1 = (id + "\t" + "RefSeq" + "\t" + "gene" + "\t" + str(x) +  "\t" + str(next)\
-					+ "\t" + "." + "\t" + "+" + "\t" + "." + "\t" + "ID=" + id + "_" + str(x) + ";Name=" + id + "_" + str(x) + "\n")
+					+ "\t" + "." + "\t" + "+" + "\t" + "." + "\t" + "ID=" + id + "_" + str(x) + "_gene" + ";Name=" + id + "_" + str(x) + "\n")
 				line2 = (id + "\t" + "RefSeq" + "\t" + "mRNA" + "\t" + str(x) +  "\t" + str(next)\
-					+ "\t" + "." + "\t" + "+" + "\t" + "." + "\t" + "ID=" + id + "_" + str(x) + ";Name=" + id + "_" + str(x) + "\n")
+					+ "\t" + "." + "\t" + "+" + "\t" + "." + "\t" + "ID=" + id + "_" + str(x) + "mRNA" + ";Name=" + id + "_" + str(x) + "\n")
 				out.write(line1) # gene
 				out.write(line2) # MRNA
 
